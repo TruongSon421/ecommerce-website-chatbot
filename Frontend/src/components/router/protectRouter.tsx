5ccd049eaa8251182c71758d6c8e701af632f32f
Nhat Tien <doannhattien4@gmail.com>
2025-03-24 14:45:21 +0000

    a

--- Frontend/src/components/router/protectRouter.tsx
+++ Frontend/src/components/router/protectRouter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext'
-import { hasAdminRole } from '../utils/auth';
+import { hasAdminRole } from '../../utils/auth';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
