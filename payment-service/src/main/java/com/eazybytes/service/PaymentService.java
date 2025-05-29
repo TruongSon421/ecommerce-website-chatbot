629f595bae9d9c2ac3b2f0371e9ad4198445481c
SON <22521260@gm.uit.edu.vn>
2025-05-29 14:05:58 +0700

    test

--- payment-service/src/main/java/com/eazybytes/service/PaymentService.java
+++ payment-service/src/main/java/com/eazybytes/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.eazybytes.service;
 
 import com.eazybytes.dto.ConfirmVNPayPaymentResponseDto;
+import com.eazybytes.dto.InitiateVNPayPaymentResponseDto;
 import com.eazybytes.event.model.ProcessPaymentRequest;
 import com.eazybytes.model.Payment;
 
@@ -9,9 +10,10 @@ import java.util.Map;
 import java.util.Optional;
 
 public interface PaymentService {
-    void processPayment(ProcessPaymentRequest request);
+    InitiateVNPayPaymentResponseDto processPayment(ProcessPaymentRequest request);
     Payment getPaymentByOrderId(Long orderId);
     ConfirmVNPayPaymentResponseDto handleVNPayReturnUrl(HttpServletRequest request);
     void handleVNPayCallback(Map<String, String> vnpayParams);
     Optional<Payment> findPaymentByTransactionId(String transactionId);
+    
 }
